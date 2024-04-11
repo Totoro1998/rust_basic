d805ad83f3bbfe226d65668385c4d067cb9f8597
totoro <totoro.fu@webeye.com>
2024-04-11 11:04:43 +0800

    数据类型

---
+++ data_types/src/main.rs
@@ -0,0 +1,99 @@
+use std::io;
+
+// 整型
+// fn main() {
+//     let value_i8: u8 = 8; // 无符号
+//     let value_u8: i8 = -8; // 有符号
+//     println!("{value_i8},{value_u8}")
+// }
+
+// 浮点型
+// fn main() {
+//     let value_f64 = 2.0; // 默认为f64
+
+//     let value_f32: f32 = 3.0; // f32
+
+//     println!("{value_f64},{value_f32}")
+// }
+
+//数值运算
+// fn main() {
+//     // addition
+//     let sum = 5 + 10;
+
+//     // subtraction
+//     let difference = 95.5 - 4.3;
+
+//     // multiplication
+//     let product = 4 * 30;
+
+//     // division
+//     let quotient = 56.7 / 32.2;
+//     let truncated = -5 / 3; // 结果为 -1
+
+//     // remainder
+//     let remainder = 43 % 5;
+//     println!(
+//         "sum={},difference={},product={},quotient={},truncated={},remainder={}",
+//         sum, difference, product, quotient, truncated, remainder
+//     )
+// }
+
+// 布尔类型
+// fn main() {
+//     let value_t = true;
+//     let value_f: bool = false; // with explicit type annotation
+
+//     println!("{value_t},{value_f}")
+// }
+
+// 字符类型
+// fn main() {
+//     let c = 'z';
+//     let z: char = 'ℤ'; // with explicit type annotation
+//     let heart_eyed_cat = '😻';
+
+//     println!("{c},{z},{heart_eyed_cat}")
+// }
+
+//元祖类型
+// fn main() {
+//     let tup = (500, 6.4, 1);
+
+//     let (x, y, z) = tup;
+
+//     let first_value = tup.0;
+
+//     println!("x={},y={},z={},first_value={}", x, y, z, first_value);
+// }
+
+// 数组类型
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+
+//     let first = a[0];
+//     let second = a[1];
+//     println!("first={},second={}", first, second);
+// }
+
+// 无效的数组元素访问
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
