55276919f91a03f8e43f3592f1c7f438e317475c
totoro <totoro.fu@webeye.com>
2024-08-14 16:00:32 +0800

    init sturct method

---
+++ p_14_struct_method/src/main.rs
@@ -0,0 +1,104 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// impl Rectangle {
+//     // 结构体的方法第一个参数总是self，代表调用该方法的结构体实例。
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         rect1.area()
+//     );
+// }
+
+// impl Rectangle {
+//     // 我们可以选择将方法的名称与结构中的一个字段相同。
+//     // 我们在 rect1.width 后面加上括号时。Rust 知道我们指的是方法 width。当我们不使用圆括号时，Rust 知道我们指的是字段 width。
+//     fn width(&self) -> bool {
+//         self.width > 0
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     if rect1.width() {
+//         println!("The rectangle has a nonzero width; it is {}", rect1.width);
+//     }
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+//     // 带有更多参数的方法
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//     let rect2 = Rectangle {
+//         width: 10,
+//         height: 40,
+//     };
+//     let rect3 = Rectangle {
+//         width: 60,
+//         height: 45,
+//     };
+
+//     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+//     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+// }
+
+// // 我们可以定义不以 self 为第一参数的关联函数（因此不是方法），因为它们并不作用于一个结构体的实例。
+// impl Rectangle {
+//     // 正方形
+//     fn square(size: u32) -> Self {
+//         Self {
+//             width: size,
+//             height: size,
+//         }
+//     }
+// }
+
+// fn main() {
+//     let sq = Rectangle::square(3);
+//     dbg!(&sq);
+// }
+
+// // 可变的self
+// fn main() {
+//     let mut rect = Rectangle {
+//         width: 100,
+//         height: 100,
+//     };
+//     rect.change_size(20);
+//     dbg!(rect);
+// }
+
+// impl Rectangle {
+//     fn change_size(&mut self, size: u32) {
+//         self.width = size;
+//         self.height = size;
+//     }
+// }
