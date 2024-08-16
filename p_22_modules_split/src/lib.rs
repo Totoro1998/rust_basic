4f0d312c5d2c92f259ba67366f8dea7607e56804
totoro <totoro.fu@webeye.com>
2024-08-16 18:52:57 +0800

    代码优化

--- p_22_modules_split/src/lib.rs
+++ p_22_modules_split/src/lib.rs
@@ -5,3 +5,15 @@ pub use crate::front_of_house::hosting;
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
 }
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// pub use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+// }
