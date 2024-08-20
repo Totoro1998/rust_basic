ea12eb9250186319ea75d528dd8d7490d9de18e9
totoro <totoro.fu@webeye.com>
2024-08-20 13:59:34 +0800

    p_3_guessing_game 创建自定义类型进行有效性验证

--- p_3_guessing_game/src/main.rs
+++ p_3_guessing_game/src/main.rs
@@ -2,10 +2,99 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+// fn main() {
+//     println!("Guess the number!");
+
+//     let secret_number = rand::thread_rng().gen_range(1..=100);
+
+//     loop {
+//         println!("Please input your guess.");
+
+//         let mut guess = String::new();
+
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Failed to read line");
+
+//         let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue, // 为了进一步改善游戏性，不要在用户输入非数字时崩溃，需要忽略非数字，让用户可以继续猜测。
+//         };
+
+//         println!("You guessed: {}", guess);
+
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too small!"),
+//             Ordering::Greater => println!("Too big!"),
+//             Ordering::Equal => {
+//                 println!("You win!");
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// 我们的代码要求用户猜测一个 1 到 100 之间的数字,并接收负数
+// fn main() {
+//     println!("Guess the number!");
+
+//     let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+//     loop {
+//         println!("Please input your guess.");
+
+//         let mut guess = String::new();
+
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Failed to read line");
+
+//         let guess: i32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue, // 为了进一步改善游戏性，不要在用户输入非数字时崩溃，需要忽略非数字，让用户可以继续猜测。
+//         };
+
+//         if guess < 1 || guess > 100 {
+//             println!("The secret number will be between 1 and 100.");
+//             continue;
+//         }
+
+//         println!("You guessed: {}", guess);
+
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too small!"),
+//             Ordering::Greater => println!("Too big!"),
+//             Ordering::Equal => {
+//                 println!("You win!");
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// 创建自定义类型进行有效性验证
 fn main() {
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {value}.");
+            }
+
+            Guess { value }
+        }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
 
     loop {
         println!("Please input your guess.");
@@ -16,14 +105,16 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue, // 为了进一步改善游戏性，不要在用户输入非数字时崩溃，需要忽略非数字，让用户可以继续猜测。
         };
 
-        println!("You guessed: {}", guess);
+        let guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
