415b91bf764638676f364434cc21c61ea0794bbc
totoro <totoro.fu@webeye.com>
2024-08-23 18:08:03 +0800

    控制测试如何运行

--- p_30_test_adder/src/lib.rs
+++ p_30_test_adder/src/lib.rs
@@ -144,18 +144,74 @@ pub struct Guess {
 //     }
 // }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+// // 将Result<T, E>用于测试
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() -> Result<(), String> {
+//         if 2 + 2 == 4 {
+//             Ok(())
+//         } else {
+//             Err(String::from("two plus two does not equal four"))
+//         }
+//     }
+// }
+
+// // 显示函数输出
+// fn prints_and_returns_10(a: i32) -> i32 {
+//     println!("I got the value {a}");
+//     10
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn this_test_will_pass() {
+//         let value = prints_and_returns_10(4);
+//         assert_eq!(10, value);
+//     }
+
+//     #[test]
+//     fn this_test_will_fail() {
+//         let value = prints_and_returns_10(8);
+//         assert_eq!(5, value);
+//     }
+// }
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
-// 将Result<T, E>用于测试
+
+// 运行单个测试：cargo test one_hundred
+// 过滤运行多个测试：cargo test add cargo test tests
+// 运行被忽略的测试：cargo test -- --ignored
+// 不管是否忽略都要运行全部测试：可以运行 cargo test -- --include-ignored。
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+    #[test]
+    #[ignore] //除非特别指定否则忽略某些测试
+    fn expensive_test() {
+        assert_eq!(102, add_two(100));
     }
 }
