a49d1e2271651470b05670786555550689dc69ed
totoro <totoro.fu@webeye.com>
2024-09-03 11:31:03 +0800

    init p_47_thread_mutex

---
+++ p_47_thread_mutex/src/main.rs
@@ -0,0 +1,79 @@
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+// fn main() {
+//     let m = Mutex::new(5);
+
+//     {
+//         let mut num = m.lock().unwrap();
+//         *num = 6;
+//     }
+
+//     println!("m = {m:?}"); // m = Mutex { data: 6, poisoned: false, .. }
+// }
+
+// // 在线程间共享Mutex<T>
+// fn main() {
+//     let counter = Mutex::new(0);
+//     let mut handles = vec![];
+
+//     for _ in 0..10 {
+//         let handle = thread::spawn(move || {
+//             let mut num = counter.lock().unwrap();
+
+//             *num += 1;
+//         });
+//         handles.push(handle);
+//     }
+
+//     for handle in handles {
+//         handle.join().unwrap();
+//     }
+
+//     println!("Result: {}", *counter.lock().unwrap());
+// }
+
+// // 多线程和多所有权
+// fn main() {
+//     let counter = Rc::new(Mutex::new(0));
+//     let mut handles = vec![];
+
+//     for _ in 0..10 {
+//         let counter = Rc::clone(&counter);
+//         let handle = thread::spawn(move || {
+//             let mut num = counter.lock().unwrap();
+
+//             *num += 1;
+//         });
+//         handles.push(handle);
+//     }
+
+//     for handle in handles {
+//         handle.join().unwrap();
+//     }
+
+//     println!("Result: {}", *counter.lock().unwrap());
+// }
+
+// 使用Arc<T>
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
