7e12fcac2a4606ade591de780da0c674e2161e04
totoro <totoro.fu@webeye.com>
2024-09-03 20:29:55 +0800

    p_53_advanced_types

---
+++ p_53_advanced_types/src/main.rs
@@ -0,0 +1,14 @@
+// // 类型别名
+// fn main() {
+//     type Kilometers = i32;
+
+//     let x: i32 = 5;
+//     let y: Kilometers = 5;
+
+//     println!("x + y = {}", x + y);
+// }
+
+// // 从不返回的 never type
+// fn bar() -> ! {}
+
+// 动态大小类型和 Sized trait
